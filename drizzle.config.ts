174c31b73ad9b4f8ceebbf12e3c661ccae38251a
Egan Bisma <egan@hey.com>
2025-05-13 19:28:58 +0700

    add db url log

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "drizzle-kit";
 import path from "node:path";
 
+console.log("database url", process.env.DATABASE_URL);
+
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
